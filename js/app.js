38190bbb7bf21c2e19789225d61bde18ea175ec0
Olena <113938017+Alenalenk@users.noreply.github.com>
2024-03-18 14:47:03 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,5 @@
 (() => {
     "use strict";
-    function isWebp() {
-        function testWebP(callback) {
-            let webP = new Image;
-            webP.onload = webP.onerror = function() {
-                callback(webP.height == 2);
-            };
-            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-        }
-        testWebP((function(support) {
-            let className = support === true ? "webp" : "no-webp";
-            document.documentElement.classList.add(className);
-        }));
-    }
     const focusParagrarh = document.querySelector(".focus__content p");
     const positionPC = document.querySelector(".focus__title");
     const positionMobile = document.querySelector(".focus__button");
@@ -58,5 +45,4 @@
         console.log(buttonActive.dataset.src);
     }
     window["FLS"] = true;
-    isWebp();
 })();
